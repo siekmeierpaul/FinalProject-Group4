2241a942c11c13136069148b04c080d2fb84653e
Dan Evans <65984016+evnnns@users.noreply.github.com>
2020-11-28 14:09:48 -0600

    We predict divorcegit add .

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
-from flask import render_template 
+import numpy as np 
+import pickle
+from flask import Flask, request
+from flask import render_template
 
 app = Flask(__name__)
+
+# Load the pickled model 
+with open("pickle_model.pkl", 'rb') as file:
+    pickle_model = pickle.load(file)
 
 @app.route('/')
 def IndexRoute():
@@ -16,6 +22,21 @@ def DataPage():
 @app.route('/results')
 def ResultsPage():
     webpage = render_template("results.html")
+    return webpage
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    # get the list from the website
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)] 
+    print(final_features)
+
+
+    # make a prediction
+    prediction = pickle_model.predict(final_features)
+    print(prediction)
+
+    webpage = render_template("data.html")
     return webpage
 
 if __name__ == '__main__':
